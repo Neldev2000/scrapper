5c3a320d50a2bf71cb9f94082b60dfc65ab42754
Neldev2000 <nelsonandresvivaslobo@gmail.com>
2023-02-24 20:30:58 +0000

    Agregado scrapping por NameCom

--- name_com.py
+++ name_com.py
@@ -1,13 +1,11 @@
 import requests
 
-
-
-class GoDaddyDomain():
-    def __init__(self, api_key, api_secret):
-        self.api_key = api_key
-        self.api_secret = api_secret
+class NameDomains():
+    def __init__(self, api_token, api_username):
+        self.api_token = api_token
+        self.api_username = api_username
         self.extensiones = []
-        self.url = 'https://api.ote-godaddy.com/v1/domains/available'
+        self.url = 'https://api.dev.name.com/v4/domains:checkAvailability'
     def definir_extensiones(self, extensiones):
         self.extensiones = extensiones
     def ver_disponibilidad_dominios(self, lista_dominios):
@@ -21,10 +19,10 @@ class GoDaddyDomain():
             pass
         return ret
     
-    def _ver_disponibilidad(self,dominio, extension):
-        headers = {"Authorization" : f"sso-key {self.api_key}:{self.api_secret}", "accept" : "application/json"}
-        params = {'domain' : f"{dominio}.{extension}"}
-        response = requests.get(self.url, headers=headers, params=params)
+    def _ver_disponibilidad(self,enlaces):
+        json = {"-u" : f"{self.api_username}:{self.api_token}", "Content-Type" : "application/json"}
+        data = {'domainNames' : f"{enlaces}"}
+        response = requests.post(self.url, data=data, json=json)
         if response.status_code != 200:
             return dict()
             
@@ -33,7 +31,3 @@ class GoDaddyDomain():
 
             return {'dominio' : data['domain'], 'precio' : f"{data['price'] / (1000000*data['period'])} {data['currency']}/yr"}
         else: return dict()
-    
-        
-
-    
--- script.py
+++ script.py
@@ -1,16 +1,21 @@
-"""import requests
+import requests
 
-url = 'https://api.ote-godaddy.com/v1/domains/available'
-API_KEY = '3mM44UcgtDEiVu_Y76godsEMvwFSb9FNNKw3g'
-API_SECRET = '581ayjUYERs7rG3sjfdsSQ'
-auth = (API_KEY, API_SECRET)
-headers = {"Authorization" : "sso-key {}:{}".format(API_KEY, API_SECRET), "accept" : "application/json"}
-#newSession = requests.session()
+url = 'https://api.dev.name.com/v4/domains:checkAvailability'
+#API_KEY = '3mM44UcgtDEiVu_Y76godsEMvwFSb9FNNKw3g'
+#API_SECRET = '581ayjUYERs7rG3sjfdsSQ'
+api_username = 'nvivas-test'
+api_token = '48ffd9a63658d5f62dd133a143aa2fb6ed934c89'
+enlaces = ['test.com']
+json = {"username" : api_username, 'password' : api_token, "Content-Type" : "application/json"}
+data = {'domainNames' : f"{enlaces}"}
+print(data, json)
 
-response = requests.get(url,  headers=headers, params=  {'domain' : "un-test.online"})
+response = requests.post(url, data = data, json = json)
+print(response.json())
+#response = requests.get(url,  headers=headers, params=  {'domain' : "un-test.online"})
 
-print(response.json())"""
-
+#print(response.json())
+"""
 from godaddy import GoDaddyDomain
 from pprint import pprint
 url = ' '
@@ -22,3 +27,4 @@ godaddy = GoDaddyDomain(GODADDY_API_KEY, GODADDY_API_SECRET)
 godaddy.definir_extensiones(['net', 'com', 'tech', 'tel', 'co', 'cloud', 'email'])
 data = godaddy.ver_disponibilidad_dominios(['un-clic', '1-clic'])
 pprint(data)
+"""
